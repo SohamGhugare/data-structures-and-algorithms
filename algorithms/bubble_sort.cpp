c1fbb16768a912ee5c416bc6153112c3adfda859
SohamGhugare <soham.ghugare@gmail.com>
2023-03-13 22:06:10 +0530

    Updated bubblesort

--- algorithms/bubble_sort.cpp
+++ algorithms/bubble_sort.cpp
@@ -1,6 +1,7 @@
 /*
 <<------ BUBBLE SORT ------>>
-Bubble sort basically just sorts the array by swapping two adjacent elements. The main approach here is to send the nth largest element in the array to the end of the array in the nth iteration till the whole array is sorted. 
+Bubble sort basically just sorts the array by swapping two adjacent elements. 
+The main approach here is to send the nth largest element in the array to the end of the array in the nth iteration till the whole array is sorted. 
 
 Time Complexity: O(n^2) 
 (since we are running two `for` loops and comparing all n elements)
@@ -21,7 +22,9 @@ void bubbleSort(int arr[], int n){
 
         /*
         Inner loop for processing elements till the (n-i) th index.
-        Why (n-i)? Because the last elemet after the iteration will always be the nth largest element. So there's no need to process the nth element again. 
+        Why (n-i)? 
+        Because the last elemet after the iteration will always be the nth largest element. 
+        So there's no need to process the nth element again. 
         */  
         for(int j=0; j<n-i; j++){
             // Compare the jth and its adjacent element and swap them if needed.
