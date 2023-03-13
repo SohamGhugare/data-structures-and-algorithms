e29ce3d95500359c2ace972853475247b74ba469
SohamGhugare <soham.ghugare@gmail.com>
2023-03-13 21:51:51 +0530

    Added bubblesort

--- algorithms/bubble_sort.cpp
+++ algorithms/bubble_sort.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int &arr, int n);
+void bubbleSort(int arr[], int n);
 
-void main(){
+int main(){
     int arr[5] = {10, 7, 14, 9, 6};
-    bubbleSort(&arr, 5);
-
+    bubbleSort(arr, 5);
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
 
-void bubbleSort(int &arr, int n){
+void bubbleSort(int arr[], int n){
     for(int i=1; i<n; i++){
         for(int j=0; j<n-i; j++){
             if(arr[j]>arr[j+1]){
